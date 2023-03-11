5b00f57487b0e012374634b993c60fd1341ce6bd
Callum Slatter <callum.slatter@OCTO-MAC-XHYFYP61KH.local>
2023-03-11 17:40:46 +0000

    feat(GameOverBanner): - Create component - Display message when you win the game

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import GuessInput from "./GuessInput/GuessInput";
 import GuessTrackingComponent from "./GuessTrackingComponent/GuessTrackingComponent";
+import GameOverBanner from "../GameOverBanner/GameOverBanner";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 // Pick a random word on every pageload.
@@ -13,6 +14,7 @@ console.info({ answer });
 
 function Game() {
   const [guessHistory, setGuessHistory] = React.useState([]);
+  const [gameStatus, setGameStatus] = React.useState("ongoing");
 
   function onGuessSubmission(tentativeGuess) {
     if (guessHistory.length >= NUM_OF_GUESSES_ALLOWED) {
@@ -22,12 +24,18 @@ function Game() {
     const nextGuessHistory = [...guessHistory];
     nextGuessHistory.push(tentativeGuess);
     setGuessHistory(nextGuessHistory);
+    if (tentativeGuess === answer) {
+      setGameStatus("won")
+    }
   }
 
   return (
     <>
       <GuessTrackingComponent guessHistory={guessHistory} answer={answer} />
       <GuessInput onGuessSubmission={onGuessSubmission} />
+      {(gameStatus === "won" | "lost") ? (
+        <GameOverBanner gameStatus={gameStatus} answer={answer} guessHistory={guessHistory}></GameOverBanner>
+      ) : undefined}
     </>
   );
 }
